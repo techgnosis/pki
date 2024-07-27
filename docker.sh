6caf0b5f8d0057eb3a122066956d5f44b1cedbae
James Musselwhite <james.musselwhite@gmail.com>
2024-07-27 16:23:46 -0700

    use caddy for testing

--- docker.sh
+++ docker.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
+echo "https://github.com/caddyserver/caddy/releases/download/v2.8.4/caddy_2.8.4_linux_arm64.tar.gz"
+
 docker run -it --rm -v $PWD:/root -w /root ubuntu:latest bash
---
+++ init.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+apt update && apt install -y openssl caddy
